616c90c0dc54112cb229dfa7e2741653c3ef8347
Dragan Vanchov <dakiman@dakis-server.vanchov.dev>
2026-04-22 14:41:07 -0400

    Add pvpTalents and talentLoadoutCode to CharacterSpecialization DTO

--- app/Blizzard/DTO/CharacterSpecialization.php
+++ app/Blizzard/DTO/CharacterSpecialization.php
@@ -6,10 +6,18 @@ namespace App\Blizzard\DTO;
 
 final readonly class CharacterSpecialization
 {
+    /**
+     * @param  array<int, array{id: int, rank: int}>  $classTalents
+     * @param  array<int, array{id: int, rank: int}>  $specTalents
+     * @param  array<int, array{id: int, rank: int}>  $heroTalents
+     * @param  array<int, array{slot: int, talent_id: int, spell_id: int}>  $pvpTalents
+     */
     public function __construct(
         public string $activeSpecialization,
-        public array $classTalents,
-        public array $specTalents,
-        public array $heroTalents,
+        public array $classTalents = [],
+        public array $specTalents = [],
+        public array $heroTalents = [],
+        public array $pvpTalents = [],
+        public ?string $talentLoadoutCode = null,
     ) {}
 }
