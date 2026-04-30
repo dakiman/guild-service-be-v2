d5fc8858dcb543b84ba317056daa79766dd0f352
Dragan Vanchov <dakiman@dakis-server.vanchov.dev>
2026-04-30 18:20:05 -0400

    test(plan-5): cover getMountIndex and getMount client methods

--- tests/Unit/Blizzard/Client/BlizzardGameDataClientTest.php
+++ tests/Unit/Blizzard/Client/BlizzardGameDataClientTest.php
@@ -198,4 +198,84 @@ class BlizzardGameDataClientTest extends TestCase
 
         $this->assertNull($this->client()->getTitle(99999));
     }
+
+    public function test_get_mount_index_returns_response_in_static_namespace(): void
+    {
+        Cache::flush();
+
+        Http::fake([
+            'us.api.blizzard.com/data/wow/mount/index?*' => Http::response([
+                'mounts' => [
+                    ['id' => 6, 'name' => 'Onyxian Drake'],
+                    ['id' => 219, 'name' => 'Tawny Wind Rider'],
+                ],
+            ], 200),
+        ]);
+
+        $result = $this->client()->getMountIndex();
+
+        $this->assertSame(6, $result['mounts'][0]['id']);
+
+        Http::assertSent(function ($request) {
+            return str_contains($request->url(), 'mount/index')
+                && str_contains($request->url(), 'namespace=static-us')
+                && str_contains($request->url(), 'locale=en_GB');
+        });
+    }
+
+    public function test_get_mount_index_caches_within_ttl(): void
+    {
+        Cache::flush();
+        $callCount = 0;
+
+        Http::fake(function () use (&$callCount) {
+            $callCount++;
+
+            return Http::response(['mounts' => []], 200);
+        });
+
+        $client = $this->client();
+        $client->getMountIndex();
+        $client->getMountIndex();
+
+        $this->assertSame(1, $callCount, 'second call should be served from cache');
+    }
+
+    public function test_get_mount_returns_full_detail(): void
+    {
+        Cache::flush();
+
+        Http::fake([
+            'us.api.blizzard.com/data/wow/mount/6?*' => Http::response([
+                'id' => 6,
+                'name' => 'Onyxian Drake',
+                'source' => ['type' => 'DROP', 'name' => 'Onyxia'],
+                'summon_spell' => ['id' => 69395, 'name' => 'Onyxian Drake'],
+                'item' => ['id' => 49636],
+            ], 200),
+        ]);
+
+        $result = $this->client()->getMount(6);
+
+        $this->assertSame(6, $result['id']);
+        $this->assertSame('Onyxian Drake', $result['name']);
+        $this->assertSame(69395, $result['summon_spell']['id']);
+
+        Http::assertSent(function ($request) {
+            return str_contains($request->url(), 'mount/6')
+                && str_contains($request->url(), 'namespace=static-us')
+                && str_contains($request->url(), 'locale=en_GB');
+        });
+    }
+
+    public function test_get_mount_returns_null_on_404(): void
+    {
+        Cache::flush();
+
+        Http::fake([
+            'us.api.blizzard.com/data/wow/mount/99999?*' => Http::response(null, 404),
+        ]);
+
+        $this->assertNull($this->client()->getMount(99999));
+    }
 }
